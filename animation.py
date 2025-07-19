c0fdbeb924fa34faffffb6a492161c9955988345
aswin-1111 <aswinak1111@gmail.com>
2025-07-19 21:09:07 +0530

    added default sample_points

--- animation.py
+++ animation.py
@@ -6,11 +6,33 @@ class JarvisMarch(Scene):
         a = Axes(x_range=[-1, 101], y_range=[-1, 101], )
         # self.add(a)
 
-        for _ in range(10):
-            x = np.random.randint(0, 100)
-            y = np.random.randint(0, 100)
-            dot_location = a.c2p(x, y)
-            dot = Dot(point=dot_location, radius=0.08, color=BLUE, fill_opacity=0.1, stroke_width=2, )
-            self.add(dot)
+        dots = VGroup()
+
+        sample_dots = VGroup(
+            Dot(point=(a.coords_to_point(42, 89))),
+            Dot(point=(a.coords_to_point(62, 72))),
+            Dot(point=(a.coords_to_point(52, 60))),
+            Dot(point=(a.coords_to_point(30, 35))),
+            Dot(point=(a.coords_to_point(47, 41))),
+            Dot(point=(a.coords_to_point(29, 59))),
+            Dot(point=(a.coords_to_point(37, 86))),
+            Dot(point=(a.coords_to_point(65, 29))),
+            Dot(point=(a.coords_to_point(17, 54))),
+            Dot(point=(a.coords_to_point(50, 74))),
+            Dot(point=(a.coords_to_point(61, 56))),
+            Dot(point=(a.coords_to_point(66, 42))),
+            Dot(point=(a.coords_to_point(34, 5))),
+            )
+
+        # for _ in range(10):
+        #     x = np.random.randint(0, 100)
+        #     y = np.random.randint(0, 100)
+        #     dot_location = a.c2p(x, y)
+        #     dot = Dot(point=dot_location, radius=0.08, color=BLUE, fill_opacity=0.1, stroke_width=2)
+        #     dots.add(dot)
+
+        # self.add(dots)
+        self.add(sample_dots)
+    
 
         
