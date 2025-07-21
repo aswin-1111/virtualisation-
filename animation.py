b64719ae8f4f425000cb5a9d24b3c400288343d9
aswin-1111 <aswinak1111@gmail.com>
2025-07-21 22:30:24 +0530

    animation cmpleted(small bug remaining)

--- animation.py
+++ animation.py
@@ -1,38 +1,75 @@
 from manim import *
 import numpy as np
 
+
 class JarvisMarch(Scene):
     def construct(self):
-        a = Axes(x_range=[-1, 101], y_range=[-1, 101], )
+        a = Axes(x_range=[-1, 101], y_range=[-1, 101])
         # self.add(a)
 
-        dots = VGroup()
+        def x(dot): return a.point_to_coords(dot.get_center())[0]
+        def y(dot): return a.point_to_coords(dot.get_center())[1]
+        def point_coords(dot): return np.array([x(dot), y(dot)])
 
-        sample_dots = VGroup(
-            Dot(point=(a.coords_to_point(42, 89))),
-            Dot(point=(a.coords_to_point(62, 72))),
-            Dot(point=(a.coords_to_point(52, 60))),
-            Dot(point=(a.coords_to_point(30, 35))),
-            Dot(point=(a.coords_to_point(47, 41))),
-            Dot(point=(a.coords_to_point(29, 59))),
-            Dot(point=(a.coords_to_point(37, 86))),
-            Dot(point=(a.coords_to_point(65, 29))),
-            Dot(point=(a.coords_to_point(17, 54))),
-            Dot(point=(a.coords_to_point(50, 74))),
-            Dot(point=(a.coords_to_point(61, 56))),
-            Dot(point=(a.coords_to_point(66, 42))),
-            Dot(point=(a.coords_to_point(34, 5))),
-            )
+        def orientation(p, q, r):
+            """Return cross product of vectors pq x qr"""
+            p, q, r = point_coords(p), point_coords(q), point_coords(r)
+            return np.cross(q - p, r - q)
 
-        # for _ in range(10):
-        #     x = np.random.randint(0, 100)
-        #     y = np.random.randint(0, 100)
-        #     dot_location = a.c2p(x, y)
-        #     dot = Dot(point=dot_location, radius=0.08, color=BLUE, fill_opacity=0.1, stroke_width=2)
-        #     dots.add(dot)
+        def find_left_point(dots):
+            minn = 0
+            for i in range(1, len(dots)):
+                if x(dots[i]) < x(dots[minn]) or (x(dots[i]) == x(dots[minn]) and y(dots[i]) < y(dots[minn])):
+                    minn = i
+            return minn
 
-        # self.add(dots)
-        self.add(sample_dots)
-    
+        dots = VGroup(
+            Dot(point=a.c2p(42, 89)),
+            Dot(point=a.c2p(62, 72)),
+            Dot(point=a.c2p(52, 60)),
+            Dot(point=a.c2p(30, 35)),
+            Dot(point=a.c2p(47, 41)),
+            Dot(point=a.c2p(29, 59)),
+            Dot(point=a.c2p(37, 86)),
+            Dot(point=a.c2p(65, 29)),
+            Dot(point=a.c2p(17, 54)),
+            Dot(point=a.c2p(50, 74)),
+            Dot(point=a.c2p(61, 56)),
+            Dot(point=a.c2p(66, 42)),
+            Dot(point=a.c2p(34, 5)),
+        )
+        self.add(dots)
 
-        
+        n = len(dots)
+        hull = []
+        l = find_left_point(dots)
+        p = l
+
+        while True:
+            hull.append(p)
+            q = (p + 1) % n
+
+            self.play(dots[p].animate.set_color(RED), run_time=0.2)
+
+            for r in range(n):
+                if r == p or r == q:
+                    continue
+
+                check_line = Line(
+                    dots[p].get_center(), dots[r].get_center(), color=BLUE, stroke_opacity=0.4
+                )
+                self.add(check_line)
+                self.wait(0.05)
+                self.remove(check_line)
+
+                if orientation(dots[p], dots[q], dots[r]) < 0:
+                    q = r
+
+            edge = Line(dots[p].get_center(), dots[q].get_center(), color=YELLOW)
+            self.play(Create(edge), run_time=0.3)
+
+            p = q
+            if p == l:
+                break
+
+        self.wait(1)
